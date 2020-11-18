5893409b3174dabfcc01beda0ab46cefe88c8e93
Damiano Falcioni <damianofalcioni@users.noreply.github.com>
2020-11-18 14:46:01 +0100

    Copyright update

--- ipb.php
+++ ipb.php
@@ -1,7 +1,7 @@
 <?php
 /*
     IP-Biter: The Hacker-friendly Tracking Framework
-    Copyright (C) 2017-2019  Damiano Falcioni (damiano.falcioni@gmail.com)
+    Copyright (C) 2017-2020  Damiano Falcioni (damiano.falcioni@gmail.com)
     
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
